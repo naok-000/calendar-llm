40fbbc0db01403b75c2f664895c18f05169acc98
naok-000 <wt.94p.1978@s.thers.ac.jp>
2025-04-18 17:03:18 +0900

    app.pyを整理

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import os
 from langchain_community.utilities import SQLDatabase
 from langchain_community.agent_toolkits import create_sql_agent
 import dotenv
+from langchain_community.utilities import SQLDatabase
+from langchain_openai import ChatOpenAI
 
-# Load environment variables from .env file
+# 環境変数の読み込み
 dotenv.load_dotenv()
 
 def _set_if_undefined(var: str):
@@ -14,24 +16,19 @@ def _set_if_undefined(var: str):
 _set_if_undefined("OPENAI_API_KEY")
 _set_if_undefined("LANGCHAIN_API_KEY")
 
-# Optional, add tracing in LangSmith
+# 設定の追加
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_PROJECT"] = "Database Agent"
 
-from langchain_community.utilities import SQLDatabase
-
+# DBを指定
 db = SQLDatabase.from_uri("sqlite:///calendar.db", sample_rows_in_table_info=3)
-
-from langchain_openai import ChatOpenAI
-
+# LLMの指定
 llm = ChatOpenAI(model="gpt-3.5-turbo")
-
+# SQLエージェントの作成
 agent_executor = create_sql_agent(llm, db=db, agent_type="openai-tools", verbose=True)
 
-
-
 if __name__ == "__main__":
+    # ユーザーからの質問を受け取り、エージェントに渡す
     prompt = input("質問を入力してください: ")
     agent_executor.invoke(prompt)
     
-
